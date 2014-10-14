846a8e24c737fe9fec3a131f3e685d85f4a8a4d8
Gordon Innes <gordski2k@yahoo.com>
2014-10-14 23:35:55 +0100

    Converted to a service.

--- app/src/main/java/net/gordski/calfaceconnector/CalFaceConnector.java
+++ app/src/main/java/net/gordski/calfaceconnector/CalFaceConnector.java
@@ -1,44 +1,95 @@
 package net.gordski.calfaceconnector;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.IBinder;
 import android.provider.CalendarContract;
+import android.text.format.Time;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.getpebble.android.kit.PebbleKit;
+import com.getpebble.android.kit.util.PebbleDictionary;
+
+import java.util.UUID;
 
 /**
  * Created by gordski on 14/09/2014.
  */
 public class CalFaceConnector extends Service
 {
-    Handler             m_main_handler;
-    CalendarObserver    m_observer;
-    Cursor              m_cal_db_cursor;
+    static final UUID PEBBLE_APP_UUID = UUID.fromString("5837b1f3-ae4c-4fbd-bafd-d90985cb0dc2");
+
+    protected android.content.BroadcastReceiver pebble_handler;
 
     @Override
     public void onCreate()
     {
         super.onCreate();
 
-        m_main_handler = new Handler();
-        m_observer = new CalendarObserver(m_main_handler);
+        Log.i("CalFaceConnector", "Service - onCreate()");
 
-        String[] projection = new String[] { CalendarContract.Events.TITLE, CalendarContract.Events.DTSTART, CalendarContract.Events.DTEND };
-        Uri events = Uri.parse("content://com.android.calendar/events");
-
-        m_cal_db_cursor = getContentResolver().query(events, projection, null, null, null);
+        pebble_handler = PebbleKit.registerReceivedDataHandler(this, new PebbleKit.PebbleDataReceiver(PEBBLE_APP_UUID)
+        {
+            @Override
+            public void receiveData(final Context context, final int transactionId, final PebbleDictionary data)
+            {
+                PebbleKit.sendAckToPebble(getApplicationContext(), transactionId);
+                sendEvents();
+            }
+        });
     }
 
+    protected void sendEvents()
+    {
+
+        String[] projection = new String[] { CalendarContract.Instances.TITLE, CalendarContract.Instances.BEGIN, CalendarContract.Instances.END };
+
+        long today = Time.getJulianDay(System.currentTimeMillis(), 0);
+        Uri events = Uri.parse(CalendarContract.Instances.CONTENT_BY_DAY_URI + "/" + today + "/" + (today + 1));
+
+        Cursor q= getContentResolver().query(events, projection, null, null, null);
+
+        PebbleDictionary event = new PebbleDictionary();
+
+        Time now = new Time();
+        now.setToNow();
+
+        while(q.moveToNext())
+        {
+            Time start = new Time();
+            start.set(q.getLong(1));
+            Time end   = new Time();
+            end.set(q.getLong(2));
+
+            if(Time.compare(end, now) >= 0)
+            {
+                event.addString(0, start.format("%H:%M") + " - " + end.format("%H:%M"));
+                event.addString(1, q.getString(0));
+
+                // Finish with the first matching event.
+                break;
+            }
+        }
+
+        PebbleKit.sendDataToPebble(getApplicationContext(), PEBBLE_APP_UUID, event);
+
+    }
 
 
     @Override
     public void onDestroy()
     {
-        if(m_cal_db_cursor != null) m_cal_db_cursor.unregisterContentObserver(m_observer);
         super.onDestroy();
+
+        Log.i("CalFaceConnector", "Service - onDestroy()");
+
+        if(pebble_handler != null) unregisterReceiver(pebble_handler);
     }
 
     @Override
@@ -50,12 +101,9 @@ public class CalFaceConnector extends Service
     @Override
     public int onStartCommand(Intent intent, int flags, int startId)
     {
-        if(m_cal_db_cursor != null) m_cal_db_cursor.registerContentObserver(m_observer);
+        Log.i("CalFaceConnector", "Service - onStart()");
+        Toast.makeText(this, "CalFaceConnector Started", Toast.LENGTH_LONG).show();
 
-        while(m_cal_db_cursor.moveToNext())
-        {
-            Log.i("Calendar", m_cal_db_cursor.getString(0));
-        }
         return START_STICKY;
     }
 
--- app/src/main/java/net/gordski/calfaceconnector/CalFaceConnectorActivity.java
+++ app/src/main/java/net/gordski/calfaceconnector/CalFaceConnectorActivity.java
@@ -1,28 +1,15 @@
 package net.gordski.calfaceconnector;
 
 import android.app.Activity;
-import android.content.Context;
-import android.database.Cursor;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
-import android.provider.CalendarContract;
-import android.text.format.Time;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
-
-import com.getpebble.android.kit.PebbleKit;
-import com.getpebble.android.kit.util.PebbleDictionary;
-
-import java.util.UUID;
 
 
 public class CalFaceConnectorActivity extends Activity
 {
 
-    static final UUID PEBBLE_APP_UUID = UUID.fromString("5837b1f3-ae4c-4fbd-bafd-d90985cb0dc2");
-
-    protected android.content.BroadcastReceiver pebble_handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -30,60 +17,15 @@ public class CalFaceConnectorActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cal_face_connector);
 
-        pebble_handler = PebbleKit.registerReceivedDataHandler(this, new PebbleKit.PebbleDataReceiver(PEBBLE_APP_UUID)
-        {
-            @Override
-            public void receiveData(final Context context, final int transactionId, final PebbleDictionary data)
-            {
-                PebbleKit.sendAckToPebble(getApplicationContext(), transactionId);
-                sendEvents();
-            }
-        });
+        Intent serviceIntent = new Intent(this, CalFaceConnector.class);
+        startService(serviceIntent);
     }
 
     @Override
     protected void onDestroy()
     {
         super.onDestroy();
-        if(pebble_handler != null) unregisterReceiver(pebble_handler);
     }
-
-    protected void sendEvents() 
-    {
-
-        String[] projection = new String[] { CalendarContract.Instances.TITLE, CalendarContract.Instances.BEGIN, CalendarContract.Instances.END };
-
-        long today = Time.getJulianDay(System.currentTimeMillis(), 0);
-        Uri events = Uri.parse(CalendarContract.Instances.CONTENT_BY_DAY_URI + "/" + today + "/" + (today + 1));
-
-        Cursor q= getContentResolver().query(events, projection, null, null, null);
-
-        TextView t= (TextView)findViewById(R.id.textView2);
-        t.setText("Events:\n");
-
-        PebbleDictionary event = new PebbleDictionary();
-
-        Time now = new Time();
-        now.setToNow();
-
-        while(q.moveToNext())
-        {
-            Time start = new Time();
-            start.set(q.getLong(1));
-            Time end   = new Time();
-            end.set(q.getLong(2));
-
-            if(Time.compare(end, now) >= 0)
-            {
-                event.addString(0, start.format("%H:%M") + " - " + end.format("%H:%M"));
-                event.addString(1, q.getString(0));
-            }
-        }
-
-        PebbleKit.sendDataToPebble(getApplicationContext(), PEBBLE_APP_UUID, event);
-
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
